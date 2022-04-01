a9df4295f16774940da6f87c7bafd0af8a3a372d
jusang9961 <99380867+jusang9961@users.noreply.github.com>
2022-04-01 10:10:41 +0900

    test

--- src/main/java/com/board/service/SmartbillNewService.java
+++ src/main/java/com/board/service/SmartbillNewService.java
@@ -8,4 +8,5 @@ public interface SmartbillNewService {
 	public void arissueDtt(SmartbillNewVO smartbillNewVO) throws Exception;
 	public void issue(SmartbillNewVO smartbillNewVO) throws  Exception;
 
+	test
 }
